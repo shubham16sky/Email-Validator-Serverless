c521299ef2347f9c6359fd45e72d81fbca2595dc
shubham16sky <shubhampc16@gmail.com>
2023-05-31 01:46:24 +0530

    first commit

---
+++ handler.py
@@ -0,0 +1,16 @@
+import json
+import re
+def validate(event,context):
+    event_body = json.loads(event['body'])
+    email_regex = re.compile('^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+    matches = email_regex.match(event_body['email']) != None
+    response = {
+        'statusCode':200,
+        'body':json.dumps({'result':matches})
+    }
+
+    return response
+
+
+
+
